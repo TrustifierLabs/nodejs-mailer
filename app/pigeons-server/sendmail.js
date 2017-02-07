a6f271c9f5f49489242d3da8f5f502ccd694d5e7
Ahmed Masud <ahmed.masud@trustifier.com>
2017-02-07 00:20:30 -0500

    renamed index.js to sendmail.js

---
+++ app/pigeons-server/sendmail.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const MailTransporter = require('./lib/mail-transporter');
+
+const mt = new MailTransporter({
+	// the json file that gmail sends you, which 
+	// contains the authentication tokens and the private key
+	// for 2LO xoauth
+	authFile: '/etc/security/google-api/service-creds.json',
+});
+
+mt.sendMail({
+	from: '"Ahmed Masud" <ahmed.masud@trustifier.com>', 
+	to: "ahmed.masud@trustifier.com", 
+	subject: "hi there you!",
+	text: "well how goes it? -- don't forget to write back"
+  });
