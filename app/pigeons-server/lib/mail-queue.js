64bd7cdc72dae22010a692fa0eb69a63e226ddfb
Ahmed Masud <ahmed.masud@trustifier.com>
2017-02-08 10:25:13 -0500

    added support for sent directory

--- app/pigeons-server/lib/mail-queue.js
+++ app/pigeons-server/lib/mail-queue.js
@@ -1,8 +1,12 @@
-'use strict;'
+'use strict';
 
+const fs = require('fs');
 
 module.exports = class MailQueue {
-	constructor({ queueDir } = { queueDir: __dirname + "/var/mail/queue" } ) {
+	constructor({ queueDir = __dirname + "/var/mail/queue" } = { queueDir: __dirname + "/var/mail/queue" } ) {
 		console.log(queueDir);
+		fs.watch(queueDir, (eventType, filename) => {
+			console.log(eventType, filename);
+		});
 	}
 };
