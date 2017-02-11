e38c8ef4a215fea33e364312d3c45d7492538410
Ahmed Masud <ahmed.masud@trustifier.com>
2017-02-11 02:49:53 -0500

    initial commit for mail queue processing

--- app/pigeons-server/lib/mail-queue.js
+++ app/pigeons-server/lib/mail-queue.js
@@ -1,12 +1,43 @@
 'use strict';
 
 const fs = require('fs');
+const watchr = require('watchr');
+const emailFileFilter = /\/email-[0-9a-fA-F]+\.json$/i;
 
-module.exports = class MailQueue {
-	constructor({ queueDir = __dirname + "/var/mail/queue" } = { queueDir: __dirname + "/var/mail/queue" } ) {
-		console.log(queueDir);
-		fs.watch(queueDir, (eventType, filename) => {
-			console.log(eventType, filename);
+module.exports = class MailQueue
+{
+	constructor({ queueDir = __dirname + "/var/mail/queue" } 
+			= { queueDir: __dirname + "/var/mail/queue" } )
+	{
+		this.queueDir = queueDir;
+		this.watcher = watchr.open(this.queueDir, this.process, () => {
+			console.log('watcher ready for ', this.queueDir);
 		});
+		
+	}
+
+	start() {
+		this.watcher.watch(() => {
+			console.log('watching ', this.queueDir);
+		});
+	}
+
+	stop() {
+		this.watcher.close();
+	}
+	
+	process(changeType, fullPath, currentStat, previousStat) {
+
+		if(changeType != "create") 
+			return; 
+
+		if(!emailFileFilter.test(fullPath))
+			return; 
+		
+		
+	}
+
+	log(...any) {
+		console.log(any);
 	}
 };
